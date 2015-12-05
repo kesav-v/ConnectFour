f4ac98db62242c757e6a95188da2cf1a5633be18
chess123 <kesav@viswanadha.net>
2015-12-04 18:49:21 -0800

    Added Connect Four Solver

---
+++ SolveConnectFour.java
@@ -0,0 +1,195 @@
+import java.io.PrintWriter;
+import java.io.File;
+import java.util.ArrayList;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SolveConnectFour implements ActionListener {
+
+	static PrintWriter output;
+	static int wins, losses, draws;
+	static CFPanel panel;
+	static Timer timer;
+	static int times;
+
+	public SolveConnectFour() {
+		timer = new Timer(1000, this);
+		timer.start();
+		try {
+			output = new PrintWriter(new File("ConnectFourSolution.txt"));
+		} catch (Exception e) {}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		times++;
+		// System.out.println("Checked " + (wins + losses + draws) + " nodes at time = " + times);
+	}
+
+	public static void main(String[] args) throws Throwable {
+		SolveConnectFour scf = new SolveConnectFour();
+		char[][] board = new char[6][7];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = ' ';
+			}
+		}
+		solve(board, true, 0, 5, 0);
+	}
+
+	public static char solve(char[][] board, boolean turn, int turns, int x, int y) throws Throwable {
+		char result = gameOver(board, x, y);
+		// if (turns == 25) System.out.println(x + "\t" + y);
+		if (turns > 0) {
+			// for (int i = 0; i < turns; i++) {
+				//System.out.print("  ");
+			// }
+			//System.out.println("(" + x + ", " + y + ")");
+		}
+		if (result == 'R' || result == 'Y') {
+			// for (int i = 0; i < turns; i++) {
+				//System.out.print("  ");
+			// }
+			//System.out.println(result + " wins");
+			if (result == 'R') wins++;
+			else losses++;
+			return result;
+		}
+		if (turns == board.length * board[0].length) {
+			if (result == 'R' || result == 'Y') {
+				// for (int i = 0; i < turns; i++) {
+					//System.out.print("  ");
+				// }
+				if (result == 'R') wins++;
+				else losses++;
+				//System.out.println(result + " wins");
+			}
+			else {
+				// for (int i = 0; i < turns; i++) {
+					//System.out.print("  ");
+				// }
+				//System.out.println("tie");
+				draws++;
+			}
+			return result;
+		}
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		for (int i = 0; i < board[0].length; i++) {
+			int bottom;
+			for (bottom = board.length - 1; bottom >= 0; bottom--) {
+				if (board[bottom][i] == ' ') break;
+			}
+			if (bottom == -1) continue;
+			if (turn) board[bottom][i] = 'R';
+			else board[bottom][i] = 'Y';
+			// Thread.sleep(100);
+			int code = (int)(solve(board, !turn, turns + 1, bottom, i));
+			if (results.indexOf(code) < 0) results.add(code);
+			board[bottom][i] = ' ';
+			if (turn && code == 'R') {
+				return 'R';
+			}
+			if (!turn && code == 'Y') {
+				return 'Y';
+			}
+		}
+		if (turn) {
+			return (char)(min(results));
+		}
+		else {
+			return (char)(max(results));
+		}
+	}
+
+	public static int min(ArrayList<Integer> results) {
+		int min = results.get(0);
+		for (int i : results) {
+			if (i < min) min = i;
+		}
+		return min;
+	}
+
+	public static int max(ArrayList<Integer> results) {
+		int max = results.get(0);
+		for (int i : results) {
+			if (i > max) max = i;
+		}
+		return max;
+	}
+
+  public static char gameOver(char[][] tboard, int x, int y) {
+    int countConsecutive = 0;
+    char color = ' ';
+    int i, a;
+    
+    for (i = x - 3; i <= x + 3; i++) // Horizontal
+      if (i >= 0 && i < tboard.length && countConsecutive < 4)
+        if (tboard[i][y] == color)
+          countConsecutive++;
+        else if (tboard[i][y] == 'R' || tboard[i][y] == 'Y') {
+          color = tboard[i][y];
+          countConsecutive = 1;
+        }
+        else	color = ' ';
+      else if (countConsecutive == 4)
+        return color;
+    if (countConsecutive == 4)
+      return color;
+    
+    countConsecutive = 0;
+    color = ' ';
+    
+    for (a = y - 3; a <= y + 3; a++) // Vertical
+      if (a >= 0 && a < tboard.length && countConsecutive < 4)
+        if (tboard[x][a] == color)
+          countConsecutive++;
+        else if (tboard[x][a] == 'R' || tboard[x][a] == 'Y') {
+          color = tboard[x][a];
+          countConsecutive = 1;
+        }
+        else	color = ' ';
+      else if (countConsecutive == 4)
+        return color;
+    if (countConsecutive == 4)
+      return color;
+    
+    countConsecutive = 0;
+    color = ' ';
+    
+    for (i = x - 3, a = y - 3; i <= x + 3; i++, a++) // diagonal 1 topleft - bottomright
+      if (a >= 0 && a < tboard.length && i >= 0 && i < tboard.length && countConsecutive < 4)
+        if (tboard[i][a] == color)
+          countConsecutive++;
+        else if (tboard[i][a] == 'R' || tboard[i][a] == 'Y') {
+          color = tboard[i][a];
+          countConsecutive = 1;
+        }
+        else	color = ' ';
+      else if (countConsecutive == 4)
+        return color;
+    if (countConsecutive == 4)
+      return color;
+    
+    countConsecutive = 0;
+    color = ' ';
+    
+    for (i = x - 3, a = y + 3; i <= x + 3; i++, a--) // diagonal 1 topright - bottomleft
+      if (a >= 0 && a < tboard.length && i >= 0 && i < tboard.length && countConsecutive < 4)
+        if (tboard[i][a] == color)
+          countConsecutive++;
+        else if (tboard[i][a] == 'R' || tboard[i][a] == 'Y') {
+          color = tboard[i][a];
+          countConsecutive = 1;
+        }
+        else	color = ' ';
+      else if (countConsecutive == 4)
+        return color;
+    if (countConsecutive == 4)
+      return color;
+    
+    for (i = 0; i < tboard.length; i++)
+      if (tboard[i][0] == ' ')
+        return 'f';
+    
+    return 't';
+  }
+}
---
+++ TestConnectFour.java
@@ -0,0 +1,52 @@
+public class TestConnectFour {
+	public static void main(String[] args) throws Throwable {
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Enter a valid position string: ");
+		String position = kb.nextLine();
+		char[][] board = setUp(position);
+		for (char[] row : board) {
+			for (char c : row) {
+				System.out.print(c + " ");
+			}
+			System.out.println();
+		}
+		boolean turn = (position.length() % 2 == 0);
+		long k = System.nanoTime();
+		System.out.println("\n" + SolveConnectFour.solve(board, turn, position.length(), lastX(position, board), lastY(position, board)) + "\n");
+		System.out.println((double)(System.nanoTime() - k) / 1E9 + " seconds to run");
+	}
+
+	public static char[][] setUp(String s) {
+		char[][] board = new char[6][7];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = ' ';
+			}
+		}
+		int[] gravRow = new int[7];
+		for (int i = 0; i < gravRow.length; i++) {
+			gravRow[i] = 5;
+		}
+		boolean turn = true;
+		for (int i = 0; i < s.length(); i++) {
+			int n = Integer.parseInt(s.substring(i, i + 1));
+			if (turn) board[gravRow[n - 1]][n - 1] = 'R';
+			else board[gravRow[n - 1]][n - 1] = 'Y';
+			gravRow[n - 1]--;
+			turn = !turn;
+		}
+		return board;
+	}
+
+	public static int lastX(String s, char[][] board) {
+		int n = Integer.parseInt(s.substring(s.length() - 1, s.length())) - 1;
+		for (int i = board.length - 1; i >= 0; i--) {
+			if (board[i][n] == ' ') return i + 1;
+		}
+		return 0;
+	}
+
+	public static int lastY(String s, char[][] board) {
+		return Integer.parseInt(s.substring(s.length() - 1, s.length())) - 1;
+	}
+}

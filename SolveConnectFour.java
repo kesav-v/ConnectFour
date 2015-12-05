e749f68b7e1f867b7af3e2675938232a5d7f738f
Ofek Gila <flamingo.ofek@gmail.com>
2015-12-05 00:22:08 -0800

    fix game_over function

--- SolveConnectFour.java
+++ SolveConnectFour.java
@@ -22,7 +22,6 @@ public class SolveConnectFour implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		times++;
-		// System.out.println("Checked " + (wins + losses + draws) + " nodes at time = " + times);
 	}
 
 	public static void main(String[] args) throws Throwable {
@@ -35,8 +34,7 @@ public class SolveConnectFour implements ActionListener {
 		solve(board, true, 0, 5, 0);
 	}
 
-// <<<<<<< HEAD
-// 	public static String[] solve(char[][] board, boolean turn, int turns, int x, int y) throws Throwable {
+	// 	public static String[] solve(char[][] board, boolean turn, int turns, int x, int y) throws Throwable {
 // 		char result = gameOver(board, x, y);
 // 		// if (turns == 25) System.out.println(x + "\t" + y);
 // 		// if (turns > 0) {
@@ -95,7 +93,8 @@ public class SolveConnectFour implements ActionListener {
 // 			}
 // 		}
 // 		return new String[] {best, (bestIndex + 1) + "" + prevStr};
-	public static int[] solve(char[][] board, boolean turn, int turns, int x, int y) throws Throwable {
+
+	public static int[] solve(char[][] board, boolean turn, int turns, int x, int y) {
 		if (turns == board.length * board[0].length) {
 			draws++;
 			return new int[] {0, y};
@@ -104,6 +103,7 @@ public class SolveConnectFour implements ActionListener {
 		char color = turn ? 'R':'Y';
 
 		int result = winningMove(board, color);
+
 		if (result >= 0)
 			if (turn) {
 				wins++;
@@ -166,6 +166,14 @@ public class SolveConnectFour implements ActionListener {
 		return -1;
 	}
 
+	public static void printBoard(char[][] tboard) {
+		for (char[] row : tboard) {
+			for (char c : row)
+				System.out.print(c + " ");
+			System.out.println();
+		}
+	}
+
 
   public static char gameOver(char[][] tboard, int x, int y) {
     int countConsecutive = 0;
@@ -190,7 +198,7 @@ public class SolveConnectFour implements ActionListener {
     color = ' ';
     
     for (a = y - 3; a <= y + 3; a++) // Vertical
-      if (a >= 0 && a < tboard.length && countConsecutive < 4)
+      if (a >= 0 && a < tboard[0].length && countConsecutive < 4)
         if (tboard[x][a] == color)
           countConsecutive++;
         else if (tboard[x][a] == 'R' || tboard[x][a] == 'Y') {
@@ -207,7 +215,7 @@ public class SolveConnectFour implements ActionListener {
     color = ' ';
     
     for (i = x - 3, a = y - 3; i <= x + 3; i++, a++) // diagonal 1 topleft - bottomright
-      if (a >= 0 && a < tboard.length && i >= 0 && i < tboard.length && countConsecutive < 4)
+      if (a >= 0 && a < tboard[0].length && i >= 0 && i < tboard.length && countConsecutive < 4)
         if (tboard[i][a] == color)
           countConsecutive++;
         else if (tboard[i][a] == 'R' || tboard[i][a] == 'Y') {
@@ -224,7 +232,7 @@ public class SolveConnectFour implements ActionListener {
     color = ' ';
     
     for (i = x - 3, a = y + 3; i <= x + 3; i++, a--) // diagonal 1 topright - bottomleft
-      if (a >= 0 && a < tboard.length && i >= 0 && i < tboard.length && countConsecutive < 4)
+      if (a >= 0 && a < tboard[0].length && i >= 0 && i < tboard.length && countConsecutive < 4)
         if (tboard[i][a] == color)
           countConsecutive++;
         else if (tboard[i][a] == 'R' || tboard[i][a] == 'Y') {
